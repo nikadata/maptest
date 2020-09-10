3a8c507dcd6d11d707ad9145e679244f7e8f419d
Nikoloz Kobakhidze <NKobakhidze@molhsa.local>
2020-09-10 15:42:43 +0400

    Moidifed open master

--- resources/views/layouts/open_master.blade.php
+++ resources/views/layouts/open_master.blade.php
@@ -89,7 +89,7 @@
       <!-- Fiscal tables -->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Fiscal
+          Test Fiscal
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="{{ route('open_fiscal') }}">1. Fiscal data</a>
